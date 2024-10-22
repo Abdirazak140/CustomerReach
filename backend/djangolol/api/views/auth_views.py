5753554674736a214c42ab19e682e98acaad57c5
alia720 <68617399+alia720@users.noreply.github.com>
2024-10-22 16:14:26 -0600

    Add Auth
    Started adding user auth for the Django backend

--- backend/djangolol/api/views/auth_views.py
+++ backend/djangolol/api/views/auth_views.py
@@ -3,3 +3,36 @@ import json
 from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
 from backend.djangolol.api.models import auth_models
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.views.decorators.http import require_POST
+
+@require_POST
+def login(request):
+    data = json.loads(request.body)
+    username = data.get("username")
+    password = data.get("password")
+
+    if username is None or password is None:
+        return JsonResponse({"detail": "Provide a username and password please!"})
+    user = authenticate(username=username, password=password)
+    if user is None:
+        return JsonResponse({"detail":"Invalid credentials!"}, status = 400)
+    login(request, user)
+    return JsonResponse({"details":"Logged in successfully"})
+
+def logout(request):
+    if not request.user.is_authenticated:
+        return JsonResponse({"details":"Not logged in!"}, status = 400)
+    logout(request)
+    return JsonResponse({"details":"Logged out successfully"})
+
+@ensure_csrf_cookie
+def session(request):
+    if not request.user.is_authenticated:
+        return JsonResponse({"isauthenticated":False})
+    return JsonResponse({"isauthenticated":True})
+
+def whoami(request):
+    if not request.user.is_authenticated:
+        return JsonResponse({"isauthenticated":False})
+    return JsonResponse({"username":request.user.username})
