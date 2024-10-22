6324a460b0ba15194da8f7efcb2a20a55203f345
alia720 <68617399+alia720@users.noreply.github.com>
2024-10-22 16:43:56 -0600

    Revert "Add Auth"
    This reverts commit 5753554674736a214c42ab19e682e98acaad57c5.

--- backend/djangolol/api/urls/auth_urls.py
+++ backend/djangolol/api/urls/auth_urls.py
@@ -1,10 +1,2 @@
 from django.urls import path
 from backend.djangolol.api.views import auth_views
-
-urlpatterns = [
-    path("login/", auth_views.login, name="api_login"),
-    path("logout/", auth_views.logout, name="api_logout"),
-    path("session/", auth_views.session, name="api_session"),
-    path("whoami/", auth_views.whoami, name="api_whoami"),
-
-]   
