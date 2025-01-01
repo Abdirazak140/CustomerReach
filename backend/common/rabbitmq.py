c01353fd131e34777285092319346ecdc274412f
Abdirazak Yusuf <abdirazak140@gmail.com>
2024-12-31 21:10:17 -0500

    Update rabbitmq.py

--- backend/common/rabbitmq.py
+++ backend/common/rabbitmq.py
@@ -1,11 +1,10 @@
 import pika
-from main_settings import RABBITMQ_HOST, RABBITMQ_PORT
 
 def start_connection():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost", port=5672))
     return connection
 
-def publish_message(queue_name, body, reply_to_queue_name):
+def publish_message(queue_name, body, reply_to_queue_name=None):
     connection = start_connection()
     channel = connection.channel()
     
@@ -16,11 +15,12 @@ def publish_message(queue_name, body, reply_to_queue_name):
     connection.close()
 
 def on_message(queue_name, handleRequest):
+    print(f'Started consuming {queue_name}')
+    
     connection = start_connection()
     channel = connection.channel()
     
     def callback(channel, method, properties, body):
-        
         response = handleRequest(body)
         
         if properties.reply_to is not None:
@@ -29,7 +29,7 @@ def on_message(queue_name, handleRequest):
     channel.queue_declare(queue=queue_name)
     channel.basic_consume(queue=queue_name, auto_ack=True, on_message_callback=callback)
     
-    connection.start_consuming()
+    channel.start_consuming()
 
 def reply(channel, properties, response):
     channel.basic_publish(exchange='', body=response, properties=pika.BasicProperties(correlation_id = properties.correlation_id))
