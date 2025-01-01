08dec8beacddf93cf47e447033c49b56e6214823
Abdirazak Yusuf <abdirazak140@gmail.com>
2024-12-31 21:10:39 -0500

    Tested rabbitmq server and methods

---
+++ backend/DBService/DBService/handlers/handle_user_data.py
@@ -0,0 +1,4 @@
+def handle_user_data(data):
+    print(f'Recieved {data}')
+
+    return "Success"
--- backend/DBService/manage.py
+++ backend/DBService/manage.py
@@ -2,7 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import multiprocessing
+from DBService.handlers import handle_user_data
+import time
 
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
+sys.path.append(ROOT_DIR)
+
+from common.rabbitmq import on_message, publish_message
 
 def main():
     """Run administrative tasks."""
@@ -18,5 +25,27 @@ def main():
     execute_from_command_line(sys.argv)
 
 
+def start_consuming_user_data():
+    on_message("user_service_data", handle_user_data.handle_user_data)
+    
+def test():
+    publish_message(queue_name="user_service_data", body="Funny ali")
+    
 if __name__ == '__main__':
-    main()
+    # Django App
+    # djapp = multiprocessing.Process(target=main)
+    
+    # DB handlers
+    handler1 =  multiprocessing.Process(target=start_consuming_user_data)
+    
+    test = multiprocessing.Process(target=test)
+    
+    # djapp.start()
+    handler1.start()
+    
+    time.sleep(10)
+    
+    test.start()
+    
+    
+    
