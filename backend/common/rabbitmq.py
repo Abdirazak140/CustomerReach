c052cf31e6e3e689591a0e8f13e13d7d28cf81c6
Abdirazak Yusuf <abdirazak140@gmail.com>
2024-12-31 19:49:46 -0500

    Updated rabbitmq methods

--- backend/common/rabbitmq.py
+++ backend/common/rabbitmq.py
@@ -1,35 +1,35 @@
 import pika
-from settings import RABBITMQ_HOST, RABBITMQ_PORT
+from main_settings import RABBITMQ_HOST, RABBITMQ_PORT
 
 def start_connection():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST, port=RABBITMQ_PORT))
     return connection
 
-def publish_message(queue_name, body):
+def publish_message(queue_name, body, reply_to_queue_name):
     connection = start_connection()
     channel = connection.channel()
     
     channel.queue_declare(queue=queue_name)
     channel.basic_publish(exchange='', routing_key=queue_name, body=body, 
-                          properties=pika.BasicProperties(reply_to=queue_name))
+                          properties=pika.BasicProperties(reply_to=reply_to_queue_name))
     
     connection.close()
 
-def on_request(queue_name):
+def on_message(queue_name, handleRequest):
     connection = start_connection()
     channel = connection.channel()
     
-    def callback(ch, method, properties, body):
-        # TODO: Process request and reply
-        print(f" [x] Received {body}")
+    def callback(channel, method, properties, body):
         
-        if False:
-            reply(ch=ch, response=null, properties=properties)
+        response = handleRequest(body)
+        
+        if properties.reply_to is not None:
+            reply(channel, properties, response)
     
     channel.queue_declare(queue=queue_name)
     channel.basic_consume(queue=queue_name, auto_ack=True, on_message_callback=callback)
     
     connection.start_consuming()
 
-def reply(ch, properties, response):
-    ch.basic_publish(exchange='', body=response, properties=pika.BasicProperties(correlation_id = properties.correlation_id))
+def reply(channel, properties, response):
+    channel.basic_publish(exchange='', body=response, properties=pika.BasicProperties(correlation_id = properties.correlation_id))
